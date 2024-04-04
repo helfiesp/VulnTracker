8764ed8201c1a01aabee1e5f0366489df2062032
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-04-04 10:06:00 +0200

    Update settings.py

--- vulnerabilities/settings.py
+++ vulnerabilities/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from vulnapp import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,10 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/' 
-STATICFILES_DIRS = (
-    os.path.normpath(os.path.join(BASE_DIR, "static")),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/web/VulnTracker/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
